1e9a5359b3059382676672e6e4147bc222c9346c
Nane <kds.napha@gmail.com>
2026-05-27 00:34:38 +0900

    call agent create entity

--- apps/api/src/features/agent/application/commands/create-agent.command-handler.ts
+++ apps/api/src/features/agent/application/commands/create-agent.command-handler.ts
@@ -1,6 +1,8 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { CreateAgentCommand } from "./create-agent.command";
 import { CreateAgentResponse } from "../create-agent.response";
+import { Agent } from "../../domain/agent.entity";
+import { v4 as uuidv4 } from "uuid";
 
 @CommandHandler(CreateAgentCommand)
 export class CreateAgentCommandHandler implements ICommandHandler<
@@ -10,7 +12,12 @@ export class CreateAgentCommandHandler implements ICommandHandler<
   constructor() {}
 
   async execute(command: CreateAgentCommand): Promise<CreateAgentResponse> {
-    // call create agent domain
+    const agent = Agent.create({
+      id: uuidv4(),
+      name: command.name,
+      systemPrompt: command.systemPrompt,
+    });
+
     return {} as CreateAgentResponse;
   }
 }
