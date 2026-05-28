cd85e4ced5624f1485a28e3aa0da2914ed2db98b
Nane <kds.napha@gmail.com>
2026-05-28 20:21:31 +0900

    create agent-repository.port.ts

---
+++ apps/api/src/features/agent/application/ports/agent-repository.port.ts
@@ -0,0 +1,8 @@
+import { Agent } from "../../domain/agent.entity";
+
+export const AGENT_REPOSITORY = Symbol("IAgentRepository");
+
+export interface IAgentRepository {
+  save(agent: Agent): Promise<void>;
+  //findById(id: string): Promise<Agent | null>;
+}
