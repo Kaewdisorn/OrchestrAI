5e0e910c7dc4327f41a9d718ad1f1b56a9e27e3d
Nane <kds.napha@gmail.com>
2026-05-30 03:29:37 +0900

    call AGENT_REPOSITORY

--- apps/api/src/features/agent/application/commands/create-agent.command-handler.ts
+++ apps/api/src/features/agent/application/commands/create-agent.command-handler.ts
@@ -3,13 +3,21 @@ import { CreateAgentCommand } from "./create-agent.command";
 import { CreateAgentResponse } from "../create-agent.response";
 import { Agent } from "../../domain/agent.entity";
 import { v4 as uuidv4 } from "uuid";
+import { Inject } from "@nestjs/common";
+import {
+  AGENT_REPOSITORY,
+  IAgentRepository,
+} from "../ports/outbound/agent-repository.port";
 
 @CommandHandler(CreateAgentCommand)
 export class CreateAgentCommandHandler implements ICommandHandler<
   CreateAgentCommand,
   CreateAgentResponse
 > {
-  constructor() {}
+  constructor(
+    @Inject(AGENT_REPOSITORY)
+    private readonly agentRepository: IAgentRepository,
+  ) {}
 
   async execute(command: CreateAgentCommand): Promise<CreateAgentResponse> {
     const agent = Agent.create({
@@ -17,6 +25,10 @@ export class CreateAgentCommandHandler implements ICommandHandler<
       name: command.name,
       systemPrompt: command.systemPrompt,
     });
+
+    await this.agentRepository.save(agent);
+
+    console.log("Agent created with ID:", agent.id);
 
     return {} as CreateAgentResponse;
   }
