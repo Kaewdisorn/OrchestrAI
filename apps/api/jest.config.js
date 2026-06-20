9bab2e3c39cf489c9426159e2ecab2e8f7515e5c
Nane <kds.napha@gmail.com>
2026-06-20 12:39:24 +0900

    convert jest config to .js

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,10 +1,7 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testMatch: ['**/*.spec.ts', '**/*.test.ts'],
 };
-
-export default config;
